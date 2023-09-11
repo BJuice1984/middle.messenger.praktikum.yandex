16bfd21bec3ad0e999f6055b68ad712fa24667b3
BJuice <beetle_juice84@mail.ru>
2023-09-11 19:45:58 +0300

    fix/setup config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '../public/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.scss";
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "../public/vite.svg";
+import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
