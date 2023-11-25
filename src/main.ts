2dd799550dc6b81259e0a9216359f7a43ca32cd8
BJuice <beetle_juice84@mail.ru>
2023-11-25 09:53:16 +0300

    feat: add bottomBar & scss

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ Object.entries(Components).forEach(([name, component]) => {
 })
 
 function navigate(page: string) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const [source, context] = pages[page]
     const container = document.getElementById('app')!
 
