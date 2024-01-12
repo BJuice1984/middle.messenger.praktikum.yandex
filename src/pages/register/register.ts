e8f96d043aab80d825ac785f2ff1918c847eb9a3
BJuice1984 <beetle_juice84@mail.ru>
2024-01-12 19:04:19 +0300

    feat(register.ts): as Block

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,9 +1,57 @@
 import Block from '../../core/Block.ts'
 import template from './register.hbs'
+import { render } from '../../core/render.ts'
 
 export class RegisterPage extends Block {
     constructor() {
-        super()
+        super({
+            inputs: [
+                {
+                    label: 'EMAIL',
+                    name: 'email',
+                },
+                {
+                    label: 'Login',
+                    name: 'login',
+                },
+                {
+                    label: 'First Name',
+                    name: 'first_name',
+                },
+                {
+                    label: 'Last Name',
+                    name: 'second_name',
+                },
+                {
+                    label: 'Phone Number',
+                    name: 'phone',
+                },
+                {
+                    label: 'Password',
+                    name: 'password',
+                },
+                {
+                    label: 'Confirm Password',
+                    name: 'confirm_password',
+                },
+            ],
+            buttons: [
+                {
+                    label: 'Sign up',
+                    classType: 'disabled',
+                    onClick: () => {
+                        render('chatty')
+                    },
+                },
+                {
+                    label: 'Sign in',
+                    classType: 'secondary',
+                    onClick: () => {
+                        render('login')
+                    },
+                },
+            ],
+        })
     }
 
     render() {
