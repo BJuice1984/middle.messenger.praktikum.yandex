6aadf247779d865a026a2eb5041ff1006b799334
BJuice1984 <beetle_juice84@mail.ru>
2024-01-22 16:24:51 +0300

    fix(main.ts): add Block type

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { render } from './core/render.ts'
 import { registerComponent } from './core/registerComponent.ts'
 import * as Components from './components/components.ts'
+import Block from './core/Block.ts'
 
 Object.entries(Components).forEach(([name, component]) => {
-    registerComponent(name, component)
+    registerComponent(name, component as typeof Block)
 })
 
 document.addEventListener('DOMContentLoaded', () => render('login'))
